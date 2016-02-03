458e9e6bd3ce472368f4e27330fb7ec6a1bcf64f
pascalhelbig <pascalhelbig@gmail.com>
2016-02-03 11:55:01 +0100

    render a static list of questions

--- controllers/poll.js
+++ controllers/poll.js
@@ -3,5 +3,10 @@
  * get users polls
  */
 exports.getAccountPolls = function (req, res) {
-  res.render('poll/account');
+  var polls = [
+    {_id: 1, question: 'Question 1'},
+    {_id: 2, question: 'Question 2'},
+    {_id: 3, question: 'Question 3'}
+  ];
+  res.render('poll/account', {polls: polls});
 };
