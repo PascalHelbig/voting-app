1ede422b1539f43e346e3433b1f758a35109a6c1
pascalhelbig <pascalhelbig@gmail.com>
2016-02-04 09:11:03 +0100

    on polls/account load from mongodb

--- controllers/poll.js
+++ controllers/poll.js
@@ -5,12 +5,11 @@ var Poll = require('../models/Poll');
  * get users polls
  */
 exports.getAccountPolls = function (req, res) {
-  var polls = [
-    {_id: 1, question: 'Question 1'},
-    {_id: 2, question: 'Question 2'},
-    {_id: 3, question: 'Question 3'}
-  ];
-  res.render('poll/account', {polls: polls});
+  Poll.find({user: req.user._id}, function (err, polls) {
+    if (err) return res.json(err);
+
+    res.render('poll/account', {polls: polls});
+  });
 };
 
 /**
