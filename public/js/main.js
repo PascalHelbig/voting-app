27d121a181498b792486959d7f48575600c38af8
pascalhelbig <pascalhelbig@gmail.com>
2016-02-04 11:21:06 +0100

    remove answers in polls

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,10 @@
-$(document).ready(function() {
-
-  // Place JavaScript code here...
+$(document).ready(function () {
+  var removeButtons = document.querySelectorAll('button[name="remove"]');
+  Array.prototype.forEach.call(removeButtons, function (button, i) {
+    button.addEventListener('click', function () {
+      var row = document.querySelector('#row' + i);
+      row.parentNode.removeChild(row);
+    });
+  });
 
 });
