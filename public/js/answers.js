a598e1e8e06081786e7f8f396ae35ca60fa68145
pascalhelbig <pascalhelbig@gmail.com>
2016-02-05 09:44:51 +0100

    only show answers code in edit.jade

---
+++ public/js/answers.js
@@ -0,0 +1,17 @@
+var removeButtons = document.querySelectorAll('#answers button[name="remove"]');
+lastIndex = removeButtons.length - 1;
+Array.prototype.forEach.call(removeButtons, addRemoveListener);
+
+document.querySelector('#addAnswer').addEventListener('click', function () {
+  var template = document.querySelector('#answerTemplate > div').cloneNode(true);
+  template.id = "row" + ++lastIndex;
+  addRemoveListener(template.querySelector('button'), lastIndex);
+  document.getElementById('answers').appendChild(template);
+});
+
+function addRemoveListener(button, i) {
+  button.addEventListener('click', function () {
+    var row = document.querySelector('#row' + i);
+    row.parentNode.removeChild(row);
+  });
+}
